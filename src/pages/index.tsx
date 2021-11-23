256406dd178dc3fa7d3a619065599e44985f1766
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-11-23 17:05:33 -0300

    Add integration with Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -21,12 +30,31 @@ export default function Home() {
           <p>
             Get access to all the publications
             <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Jz3arD53ztgXjgju5Srin98')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  const ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+  return {
+    props: { product },
+    revalidate: ONE_DAY_IN_SECONDS,
+  }
 }
