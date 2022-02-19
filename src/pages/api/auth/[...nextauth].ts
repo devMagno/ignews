9feee84af121324ecf1296e9ba06879f49cb114a
Guilherme Magno <guilhermertfmagno@gmail.com>
2022-02-19 10:45:20 -0300

    feat: generating stripe checkout session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import { query as q } from 'faunadb'
 
 import NextAuth from 'next-auth'
-import { signIn } from 'next-auth/client'
 import Providers from 'next-auth/providers'
 
 import { fauna } from '../../../services/fauna'
@@ -15,7 +14,7 @@ export default NextAuth({
   ],
 
   callbacks: {
-    async signIn(user, account, profile) {
+    async signIn(user) {
       const { email } = user
 
       try {
